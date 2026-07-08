9b77473c4257a576d75d5511c5917832e217da63
杜冠魁 <hello@dignite.com>
2026-07-08 16:19:53 +0900

    feat: add NotificationCenter localization resource (#11)

--- notification-center/src/Dignite.Abp.NotificationCenter.Domain.Shared/Dignite/Abp/NotificationCenter/AbpNotificationCenterDomainSharedModule.cs
+++ notification-center/src/Dignite.Abp.NotificationCenter.Domain.Shared/Dignite/Abp/NotificationCenter/AbpNotificationCenterDomainSharedModule.cs
@@ -1,5 +1,7 @@
+using Dignite.Abp.NotificationCenter.Localization;
 using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
 
 namespace Dignite.Abp.NotificationCenter;
 
@@ -8,4 +10,18 @@ namespace Dignite.Abp.NotificationCenter;
     )]
 public class AbpNotificationCenterDomainSharedModule : AbpModule
 {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<AbpNotificationCenterDomainSharedModule>();
+        });
+
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources
+                .Add<NotificationCenterResource>("en")
+                .AddVirtualJson("/Dignite/Abp/NotificationCenter/Localization");
+        });
+    }
 }
